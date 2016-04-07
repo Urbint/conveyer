e7f127c60dfcf72803ef0ad910d3ef9215d75063
Ryan Schmukler <rschmukler@gmail.com>
2016-04-07 19:46:06 -0400

    feat(explain): add explain method

---
+++ explain.go
@@ -0,0 +1,27 @@
+package conveyer
+
+import (
+	"encoding/json"
+	"github.com/smartystreets/assertions/internal/go-render/render"
+)
+
+// Explain returns a structured error message for displaying in the goconvey ui
+// it is a glorified reimplementation of assertions.serializer
+func Explain(message string, expected interface{}, actual interface{}) string {
+	view := failureView{
+		Message:  message,
+		Expected: render.Render(expected),
+		Actual:   render.Render(actual),
+	}
+	serialized, err := json.Marshal(view)
+	if err != nil {
+		return message
+	}
+	return string(serialized)
+}
+
+type failureView struct {
+	Message  string `json:"Message"`
+	Expected string `json:"Expected"`
+	Actual   string `json:"Actual"`
+}
